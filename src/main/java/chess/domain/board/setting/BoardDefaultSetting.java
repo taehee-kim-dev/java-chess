f9524315c2737337580c8a7affd78f6fc8c268cc
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-04-13 22:56:25 +0900

    refactor: 불필요한 @Primary 삭제

--- src/main/java/chess/domain/board/setting/BoardDefaultSetting.java
+++ src/main/java/chess/domain/board/setting/BoardDefaultSetting.java
@@ -17,7 +17,6 @@ import java.util.Arrays;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Primary
 @Component
 public class BoardDefaultSetting extends BoardSetting {
     public BoardDefaultSetting() {
