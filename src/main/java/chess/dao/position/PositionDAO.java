bfd86096adad038bde6291bb21944738380e3105
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-04-13 17:22:33 +0900

    feat: PositionDAO JdbcTemplate 적용

--- src/main/java/chess/dao/position/PositionDAO.java
+++ src/main/java/chess/dao/position/PositionDAO.java
@@ -17,29 +17,14 @@ public class PositionDAO implements PositionRepository {
     }
 
     @Override
-    public Position findByFileAndRank(File file, Rank rank) throws SQLException {
+    public Position findByFileAndRank(File file, Rank rank) {
         String query = "SELECT * FROM position WHERE file_value = ? AND rank_value = ?";
         return jdbcTemplate.queryForObject(
             query,
-            (resultSet, rowNum) -> {
-                Position position = new Position(
-                    resultSet.getLong("id"),
-                    resultSet.getString("file_value"),
-                    resultSet.getString("rank_value")
-                );
-                return position;
-            }, file.getValue(), String.valueOf(rank.getValue()));
+            (resultSet, rowNum) -> new Position(
+                resultSet.getLong("id"),
+                resultSet.getString("file_value"),
+                resultSet.getString("rank_value")
+            ), file.getValue(), String.valueOf(rank.getValue()));
     }
-//    @Override
-//    public Position findByFileAndRank(File file, Rank rank) throws SQLException {
-//        String query = "SELECT * FROM position WHERE file_value = ? AND rank_value = ?";
-//        ResultSet resultSet = SQLQuery.select(query, file.getValue(), String.valueOf(rank.getValue()));
-//        if (!resultSet.next()) {
-//            return null;
-//        }
-//        return new Position(
-//            resultSet.getLong("id"),
-//            resultSet.getString("file_value"),
-//            resultSet.getString("rank_value"));
-//    }
 }
